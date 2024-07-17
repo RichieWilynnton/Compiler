16e4889fdd4b46e3732b69a7aa809a658ed5b762
RichieWilynnton <richiewilynton@gmail.com>
2024-07-17 23:40:07 +0800

    demo code.txt

--- output/output.c
+++ output/output.c
@@ -2,7 +2,20 @@
 #include "UtilFunctions.h"
 int main(){
 int a = 3;
-printf("%d\n", 11);
+int b = 2;
+for (int i=0;i<10;i++){
+a = (a + b);
+};
+while (a) {
+printf("%d\n", a);
+a = (a-1);
+};
+printf("%s\n", "done");
+a = 4;
+{
+int a = 2;
+};
+printf("%d\n", a);
 
 return 0;
 }
